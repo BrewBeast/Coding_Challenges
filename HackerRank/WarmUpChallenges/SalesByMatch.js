a85d69678a8567d77b61a89aee109f7ff41cf9a7
Matthias Fore <matthiasfore@Matthiass-MacBook-Pro.local>
2020-10-27 11:47:33 -0700

    passed

--- HackerRank/WarmUpChallenges/SalesByMatch.js
+++ HackerRank/WarmUpChallenges/SalesByMatch.js
@@ -27,7 +27,31 @@ function readLine() {
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
 
+    var count = 0;
+    var color = 0;
+//sort pile to make it easier to work with 
+    ar.sort()
 
+//test size of sock pile
+    if(n < 1 || n > 100 || ar.length === 0 || ar.length > n){
+        return;
+    }
+
+    
+    for(var i = 0; i <  ar.length; i += 1){
+//test color viability
+        if(ar[i] <= 0 || ar[i] > 100){
+            return;
+//catch and record match
+        }else if(ar[i] === color){
+            count += 1;
+            color = 0;
+        }else{
+//hold a spare
+            color = ar[i];
+        }
+    }
+    return count
 }
 
 function main() {
